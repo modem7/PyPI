c9e4fec9b84c930775bc7a72b6b96e5252880aca
Alex <modem7@gmail.com>
2022-06-12 23:21:57 +0100

    updated scripts

--- build_pypi_wheels.sh
+++ build_pypi_wheels.sh
@@ -7,6 +7,9 @@
 ## Execute the registering scripts:
 ### docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
+PUID=$(id -u)
+PGID=$(id -g)
+UPLOADER=modem7
 IMG=python:3.10-alpine3.16
 SCRIPT=./dumb-pypi/dumb_pypi_wheels.sh
 PKG_URL=https://raw.githubusercontent.com/modem7/PyPI/gh-pages/packages
@@ -22,6 +25,18 @@ docker run --rm -v "$(pwd)":/data -w /data $ALPINE_ARMV7 sh -c $SCRIPT &
 docker run --rm -v "$(pwd)":/data -w /data $ALPINE_X86 sh -c $SCRIPT &
 wait
 
-docker run --rm -v "$(pwd)":/data -w /data -e PKG_URL=$PKG_URL -it modem7/dumb-pypi sh -c 'dumb-pypi --package-list <(ls packages) \
+# Create package.json
+git ls-files | xargs -I{} git log -1 --date=format:%Y%m%d%H%M.%S --format='touch -t %ad "{}"' "{}" | $SHELL
+echo -n > packages.json
+for FILE in $(ls packages | sed -e 's/"/\\"/g')
+do
+TIMESTAMP=$(date -r packages/${FILE} +%s)
+echo -en {\"filename\": \"${FILE}\", \"uploaded_by\": \"${UPLOADER}\", \"upload_timestamp\": ${TIMESTAMP}\} '\n' >> packages.json
+done
+
+# Create index
+docker run --rm --user=$PUID:$PGID -v "$(pwd)":/data -w /data -e PKG_URL=$PKG_URL -it modem7/dumb-pypi sh -c 'dumb-pypi \
+   --package-list-json packages.json \
    --packages-url $PKG_URL \
    --output-dir .'
+
