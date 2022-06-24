c547e69299956ebce62befa117666bd5f273a1a9
modem7 <modem7@gmail.com>
2022-06-24 15:56:12 +0100

    delete armv7l wheels for generic

--- build_generic_wheels.sh
+++ build_generic_wheels.sh
@@ -15,12 +15,10 @@ SCRIPT=./generic/generic_wheels.sh
 PKG_URL=https://raw.githubusercontent.com/modem7/PyPI/gh-pages/packages
 # https://github.com/docker-library/official-images#architectures-other-than-amd64
 ALPINE_X86=${IMG}
-# ALPINE_ARMV6=arm32v6/${IMG} - builds for armv6 and armv7 both come under armv7l
 # ALPINE_ARMV7=arm32v7/${IMG} # too many issues with 32bit stuff. Going to drop it
 ALPINE_ARM64=arm64v8/${IMG}
 
 docker run --rm -v "$(pwd)":/data -w /data $ALPINE_ARM64 sh -c $SCRIPT &
-# docker run --rm -v "$(pwd)":/data -w /data $ALPINE_ARMV6 sh -c $SCRIPT &
 # docker run --rm -v "$(pwd)":/data -w /data $ALPINE_ARMV7 sh -c $SCRIPT &
 docker run --rm -v "$(pwd)":/data -w /data $ALPINE_X86 sh -c $SCRIPT &
 wait
